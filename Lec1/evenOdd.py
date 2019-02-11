d1531e771d160d62be2e66db41a92f68aae9055b
Nuzair Ibrahim <nuzairb@gmail.com>
2019-02-11 14:08:08 +0500

    init

--- Lec1/evenOdd.py
+++ Lec1/evenOdd.py
@@ -1,8 +1,15 @@
-x = int(input('Enter an integer: '))
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 11 11:14:34 2019
+
+@author: nuzair
+"""
+x = int(input('Enter an interger: '))
 if x%2 == 0:
     print('')
     print('Even')
-else:
+else: 
     print('')
     print('Odd')
-print('Done with conditional')
+print('Done with conditionl')
